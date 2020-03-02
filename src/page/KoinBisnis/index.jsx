68e6b2c4a83882275f8f1a8d1ba4ff9c6f757316
putra992 <putra.irawan@koinworks.com>
2020-03-02 17:56:06 +0700

    rename-folder-roots

--- src/page/KoinBisnis/index.jsx
+++ src/page/KoinBisnis/index.jsx
@@ -4,14 +4,68 @@ import KoinBisnisVisual from "../../components/KoinBisnisKeyVisual";
 import KoinBisnisPoint from "../../components/KoinBisnisPoint";
 import KoinBisnisWhy from "../../components/KoinBisnisWhy";
 import KoinBisnisHowWorks from "../../components/KoinBisnisHowWorks";
+import HomeSliderMedia from "../../components/HomeSliderMedia";
+import HomeHowToUseKoinworks from "../../components/HomeHowToUseKoinworks";
+import KoinP2pTrusted from "../../components/KoinP2pTrusted";
 
 function KoinBisnis(props) {
+    const imageSlider = [
+        {
+          image: require("../../assets/image/how-to-use-homepage1.png"),
+          label:
+            "homeLanguage:homeHowToUseKoinworks.homeHowToUseKoinworksParagphSlideOne",
+          description:
+            "homeLanguage:homeHowToUseKoinworks.homeHowToUseKoinworksDescriptionSlideOne",
+          imagePlayStore: require("../../assets/image/google-play-download.svg"),
+          imageAppStore: require("../../assets/image/app-store-download.svg")
+        },
+        {
+          image: require("../../assets/image/how-to-use-homepage2.png"),
+          label:
+            "homeLanguage:homeHowToUseKoinworks.homeHowToUseKoinworksHeadingTwo",
+          description:
+            "homeLanguage:homeHowToUseKoinworks.homeHowToUseKoinworksDescriptionSlideTwo"
+        },
+        {
+          image: require("../../assets/image/koinp2p-image/How to Use P2P - 2.png"),
+          label: "koinp2p:koinp2p.koinp2p-content-howToUse-sliderThreeHeading",
+          description:
+            "koinp2p:koinp2p.koinp2p-content-howToUse-sliderThreeDescription"
+        },
+        {
+          image: require("../../assets/image/koinrobo-image/How-to-Use-Robo-3.png"),
+          label: "koinrobo:koinrobo.koinrobo-howToUseLabelFour",
+          description: "koinrobo:koinrobo.koinrobo-descriptionFour"
+        },
+        {
+          image: require("../../assets/image/koinrobo-image/How-to-Use-Robo-4.png"),
+          label: "koinrobo:koinrobo.koinrobo-howToUseLabelFive",
+          description: "koinrobo:koinrobo.koinrobo-descriptionFive"
+        },
+        {
+          image: require("../../assets/image/koinrobo-image/How-to-Use-Robo-5.png"),
+          label: "koinrobo:koinrobo.koinrobo-howToUseLabelSix",
+          description: "koinrobo:koinrobo.koinrobo-descriptionSix"
+        },
+        {
+          image: require("../../assets/image/koinp2p-image/How to Use P2P - 7.png"),
+          label: "koinp2p:koinp2p.koinp2p-content-howToUse-sliderEightHeading"
+        }
+      ];
   return (
     <Section>
       <KoinBisnisVisual />
       <KoinBisnisPoint />
       <KoinBisnisWhy />
       <KoinBisnisHowWorks/>
+      <HomeSliderMedia />
+        <div style={{ marginTop: 130 }}>
+          <KoinP2pTrusted />
+        </div>
+        <HomeHowToUseKoinworks
+          imageSlider={imageSlider}
+          headingTitle="koinrobo:koinrobo.koinrobo-howToUse"
+        />
     </Section>
   );
 }
