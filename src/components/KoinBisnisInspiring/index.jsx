fc583a07c1fb7f258ffbdb4fc9670b38d565def8
putra992 <putra.irawan@koinworks.com>
2020-03-03 10:33:49 +0700

    edit/meta-heading

---
+++ src/components/KoinBisnisInspiring/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+function KoinBisnisInspiring(props) {
+    return (
+        <div className="kw-koinBisnisInspiring-wrapper">
+            <Row>
+                <Col xs={24} md={13} lg={14}>
+                    <h3></h3>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default KoinBisnisInspiring;
