f3858e559dc078f299a9e0aec2f34d7cfae2c792
putra992 <putra.irawan@koinworks.com>
2020-03-09 14:20:20 +0700

    edit/service-workerv2

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,23 @@
 console.log('I am a Service Worker!');
+self.addEventListener('install', e => {
+    e.waitUntil(
+       caches.open('airhorner').then(cache => {
+           return cache.addAll([
+               '/'
+           ])
+               .then(() => self.skipWaiting());
+       })
+     )
+   });
+   
+   self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+   });
+   
+   self.addEventListener('fetch', event => {
+     event.respondWith(
+       caches.match(event.request).then(response => {
+           return response || fetch(event.request);
+       })
+     );
+   });
